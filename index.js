4454d04a53c159ee8dd7066dab54057e38514a65
Mariia1986 <renij1986@gmail.com>
2020-12-11 10:58:56 +0200

    сделано 1 дз

---
+++ index.js
@@ -0,0 +1,32 @@
+const colors = [
+    '#FFFFFF',
+    '#2196F3',
+    '#4CAF50',
+    '#FF9800',
+    '#009688',
+    '#795548',
+  ];
+  
+
+  const refs= {
+  start:document.querySelector('[data-action="start"]'),
+  stop:document.querySelector('[data-action="stop"]'),
+  body:document.querySelector('body')
+  }
+
+  const randomIntegerFromInterval = (min, max) => {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  };
+  let colorGen=null;
+
+  function changeColor() {
+      refs.body.style.backgroundColor=colors[randomIntegerFromInterval(0,5)];
+  }
+  refs.start.addEventListener('click',(event)=>{ colorGen=setInterval(changeColor,1000)
+    event.target.disabled = true;
+    console.dir(event.target)
+  })
+  refs.stop.addEventListener('click',()=>{clearInterval(colorGen)
+    refs.start.disabled=false;
+
+})
