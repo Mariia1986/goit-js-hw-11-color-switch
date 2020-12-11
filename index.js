e7625f38f5cbf38b6f1531ca3954909140b037b7
Mariia1986 <renij1986@gmail.com>
2020-12-11 11:42:12 +0200

    ввв

--- index.js
+++ index.js
@@ -1,32 +1,32 @@
 const colors = [
-    '#FFFFFF',
-    '#2196F3',
-    '#4CAF50',
-    '#FF9800',
-    '#009688',
-    '#795548',
-  ];
-  
+  "#FFFFFF",
+  "#2196F3",
+  "#4CAF50",
+  "#FF9800",
+  "#009688",
+  "#795548",
+];
 
-  const refs= {
-  start:document.querySelector('[data-action="start"]'),
-  stop:document.querySelector('[data-action="stop"]'),
-  body:document.querySelector('body')
-  }
+const refs = {
+  start: document.querySelector('[data-action="start"]'),
+  stop: document.querySelector('[data-action="stop"]'),
+  body: document.querySelector("body"),
+};
 
-  const randomIntegerFromInterval = (min, max) => {
-    return Math.floor(Math.random() * (max - min + 1) + min);
-  };
-  let colorGen=null;
+const randomIntegerFromInterval = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+let colorGen = null;
 
-  function changeColor() {
-      refs.body.style.backgroundColor=colors[randomIntegerFromInterval(0,colors.length)];
-  }
-  refs.start.addEventListener('click',(event)=>{ colorGen=setInterval(changeColor,1000)
-    event.target.disabled = true;
-    console.dir(event.target)
-  })
-  refs.stop.addEventListener('click',()=>{clearInterval(colorGen)
-    refs.start.disabled=false;
-
-})
+function changeColor() {
+  refs.body.style.backgroundColor =
+    colors[randomIntegerFromInterval(0, colors.length)];
+}
+refs.start.addEventListener("click", (event) => {
+  colorGen = setInterval(changeColor, 1000);
+  event.target.disabled = true;
+});
+refs.stop.addEventListener("click", () => {
+  clearInterval(colorGen);
+  refs.start.disabled = false;
+});
